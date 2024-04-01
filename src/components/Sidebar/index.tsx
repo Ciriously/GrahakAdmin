690032a4e42caa064b6723518b53bfb70ea7f9cf
Aditya Mishra <56506651+Ciriously@users.noreply.github.com>
2024-04-02 01:22:07 +0530

    Feat : Sidebar init

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -87,7 +87,10 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <NavLink to="/">
-          <img src={Logo} alt="Logo" />
+          <div className="flex items-center">
+            <img src={Logo} alt="Logo" width={223} />
+            {/* <p>Company Name</p> */}
+          </div>
         </NavLink>
 
         <button
